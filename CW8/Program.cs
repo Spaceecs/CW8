05170683a2491c5971ecba69fcde5da0a9f96a64
Spaceecs <azaza2046goi@gmail.com>
2024-01-17 20:53:53 +0200

    FindMax

--- CW8/Program.cs
+++ CW8/Program.cs
@@ -22,4 +22,15 @@ int[] GetEvenNumbers(int[] array)
     }
     return oddNumbersList.ToArray();
 }
-
+int FindMax(int[] array)
+{
+    int max=array[0];
+    for (int i=0;i<array.Length;++i)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
