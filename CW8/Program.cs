8647111be19bb87daa9b2f785066288f91277f5c
Spaceecs <azaza2046goi@gmail.com>
2024-01-17 20:49:39 +0200

    add GetEvenNumbers

--- CW8/Program.cs
+++ CW8/Program.cs
@@ -10,3 +10,16 @@
     }
     return oddNumbersList.ToArray();
 }
+int[] GetEvenNumbers(int[] array)
+{
+    List<int> oddNumbersList = new List<int>();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            oddNumbersList.Add(array[i]);
+        }
+    }
+    return oddNumbersList.ToArray();
+}
+
